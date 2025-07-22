40eb0867dbef806a18bb7f82f7b36cbbad2b65b2
Ilyas Hirsi <ihirsi@uwaterloo.ca>
2025-07-22 18:19:01 -0400

    added ailevel1 and updated computerplayer.h

--- computerplayer.h
+++ computerplayer.h
@@ -1,15 +1,15 @@
 #ifndef COMPUTERPLAYER_H
 #define COMPUTERPLAYER_H
-
+#include "PRNG.h"
 #include "player.h"
 #include "board.h"
-
+#include 
 class ComputerPlayer : public Player {
 public:
     ComputerPlayer(Colour c) : Player(c) {}
     bool makeMove(Board &board) override;
 
-    virtual Position determineNextBestMove(Board &b) = 0;
+    virtual std::vector<Position,2> determineNextBestMove(Board &b) = 0;
 };
 
 #endif
